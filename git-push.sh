c080c1cc3fc59f71f26a687a3414c42fbeb1c172
tudulius <tudulius@gmail.com>
2025-01-25 21:12:55 +0000

    first commit

--- git-push.sh
+++ git-push.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/tudulius/Github-Actions-S3-koreait.git
+#git remote add origin https://github.com/tudulius/Github-Actions-S3-koreait.git
 git push -u origin main
 
